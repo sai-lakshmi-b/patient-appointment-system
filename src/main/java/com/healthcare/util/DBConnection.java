9931acd06dd16773d95afc725f5e7ef1c4f60887
sai <sailakshmi.b5120@gmail.com>
2026-06-16 15:16:22 +0530

    Fix Railway public DB connection

--- src/main/java/com/healthcare/util/DBConnection.java
+++ src/main/java/com/healthcare/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL =
-            "jdbc:mysql://mysql.railway.internal:3306/railway?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+            "jdbc:mysql://thomas.proxy.rlwy.net:55926/railway?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 
     private static final String USER = "root";
 
